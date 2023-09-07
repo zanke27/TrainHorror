b9618795298bf2552369998500d7c7730c0209f4
taeyoung1472 <77605585+taeyoung1472@users.noreply.github.com>
2023-09-07 14:30:54 +0900

    작업한거

---
+++ Source/TrainHorror/Private/Monster.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Monster.h"
+
+// Sets default values
+AMonster::AMonster()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMonster::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMonster::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
