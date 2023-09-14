53d151f8373fb20a9ccd90c0ce2484a0497367d2
taeyoung1472 <77605585+taeyoung1472@users.noreply.github.com>
2023-09-14 14:59:14 +0900

    코딩 밑작업

--- Source/TrainHorror/Public/Area.h
+++ Source/TrainHorror/Public/Area.h
@@ -15,6 +15,11 @@ public:
 	// Sets default values for this actor's properties
 	AArea();
 
+	UPROPERTY(EditAnywhere)
+	class AMonster* monster;
+	UPROPERTY(EditAnywhere)
+	FVector spawnPosition;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
