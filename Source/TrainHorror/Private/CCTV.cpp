f26de443be318575aae384e2eec31af004c3a377
zanke27 <zanke27@gmail.com>
2023-09-25 16:39:48 +0900

    노이즈 효과 + 렌더 텍스쳐 제작중(버그있음)

--- Source/TrainHorror/Private/CCTV.cpp
+++ Source/TrainHorror/Private/CCTV.cpp
@@ -4,6 +4,7 @@
 #include "CCTV.h"
 #include "GameFramework/Actor.h"
 #include "Camera/CameraActor.h"
+#include "Engine/SceneCapture2D.h"
 #include <Kismet/GameplayStatics.h>
 
 // Sets default values
@@ -23,15 +24,10 @@ void ACCTV::BeginPlay()
 	TArray<AActor*> FoundActors;
 	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ACameraActor::StaticClass(), FoundActors);
 
-	UKismetSystemLibrary::PrintString(GetWorld(), TEXT("Find Start"), true, true, FLinearColor::Green, 2.0f);
-
 	for (AActor* Actor : FoundActors)
 	{
 		if (Actor->IsA(ACameraActor::StaticClass()) && Actor->GetActorLabel() == TEXT("CCTV1"))
-		{
 			cctv1 = Cast<ACameraActor>(Actor);
-			UKismetSystemLibrary::PrintString(GetWorld(), TEXT("Find CCTV1"), true, true, FLinearColor::Green, 2.0f);
-		}
 		
 		if (Actor->IsA(ACameraActor::StaticClass()) && Actor->GetActorLabel() == TEXT("CCTV2"))
 			cctv2 = Cast<ACameraActor>(Actor);
@@ -47,8 +43,48 @@ void ACCTV::BeginPlay()
 
 		if (Actor->IsA(ACameraActor::StaticClass()) && Actor->GetActorLabel() == TEXT("CCTV6"))
 			cctv6 = Cast<ACameraActor>(Actor);
+			
+		if (Actor->GetActorLabel() == TEXT("SceneCapture2D"))
+			sceneCapture2D = Cast<ASceneCapture2D>(Actor);
 	}
 	
+}
+
+void ACCTV::SetSceneScapture2DPos(int index)
+{
+	FVector locationVec;
+	FRotator rotationVec;
+	switch (index)
+	{
+		case 1:
+			locationVec = cctv1->GetActorLocation();
+			rotationVec = cctv1->GetActorRotation();
+		break;
+		case 2:
+			locationVec = cctv2->GetActorLocation();
+			rotationVec = cctv2->GetActorRotation();
+		break;
+		case 3:
+			locationVec = cctv3->GetActorLocation();
+			rotationVec = cctv3->GetActorRotation();
+		break;
+		case 4:
+			locationVec = cctv4->GetActorLocation();
+			rotationVec = cctv4->GetActorRotation();
+		break;
+		case 5:
+			locationVec = cctv5->GetActorLocation();
+			rotationVec = cctv5->GetActorRotation();
+		break;
+		case 6:
+			locationVec = cctv6->GetActorLocation();
+			rotationVec = cctv6->GetActorRotation();
+		break;
+		default:
+		break;
+	}
+	sceneCapture2D->SetActorRelativeLocation(locationVec);
+	sceneCapture2D->SetActorRelativeRotation(rotationVec);
 }
 
 // Called every frame
