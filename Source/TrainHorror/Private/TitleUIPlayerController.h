1fb7407d6c624439b6879ba5ecfe3b01b1bc57ee
zanke27 <zanke27@gmail.com>
2023-10-06 01:41:31 +0900

    타이틀 화면 추가

---
+++ Source/TrainHorror/Private/TitleUIPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TitleUIPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ATitleUIPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
